7cc7372eced265c0223a4014e7ffdae99eca5b95
skvk18 <supriya.kundu.1999@gmail.com>
2020-07-06 09:55:40 +0530

    2nd bracket removed

--- Router/student.js
+++ Router/student.js
@@ -3,24 +3,14 @@ const router = express.Router()
 const connection = require('../Database/dbConnection')
 const student = require('../Source/Student/studentOperation')
 
-router.get('/',  async(req, res) => {
-    await student.fetchAll(req,res)
-})
+router.get('/',  async(req, res) => await student.fetchAll(req,res))
 
-router.get('/:id', async(req, res) => {
-    await student.fetchUsingID(req, res)
-})
+router.get('/:id', async(req, res) => await student.fetchUsingID(req, res))
 
-router.post('/', async(req,res) => {
-    await student.insertStudent(req,res)
-})
+router.post('/', async(req,res) => await student.insertStudent(req,res))
 
-router.post('/:id', async(req,res) => {
-    await student.updateStudent(req,res)
-})
+router.post('/:id', async(req,res) => await student.updateStudent(req,res))
 
-router.delete('/:id', async(req, res) =>{
-    await student.deleteStudent(req, res)
-})
+router.delete('/:id', async(req, res) => await student.deleteStudent(req, res))
 
 module.exports = router
