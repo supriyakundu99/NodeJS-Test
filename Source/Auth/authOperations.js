523f291833e210fbb08fcc651aa32e16b6ff8126
skvk18 <supriya.kundu.1999@gmail.com>
2020-08-02 13:42:44 +0530

    Crypto check

--- Source/Auth/authOperations.js
+++ Source/Auth/authOperations.js
@@ -1,26 +1,28 @@
 const connection = require("../../Database/dbConnection");
 const uuid = require('uuid')
-// const cryptoJS = require('crypto')
+const crypto = require('crypto')
 
 module.exports = {
 
-    "login": function(req, res){
+    "login": function (req, res) {
         console.log("Login called...")
-        console.log("uuid: ",uuid.v4())
+        console.log("uuid: ", uuid.v4())
         qString = 'SELECT * FROM auth_users where (user_name = ?);'
         qUser = req.body.userName
         connection.query(qString, [qUser], (err, rows, fields) => {
-            if(!err){
-                if( rows.length  == 0){
-                    console.log("Rows: "+ rows)
+            if (!err) {
+                if (rows.length == 0) {
+                    console.log("Rows: " + rows)
                     res.send("No user found")
                 }
-                else{
+                else {
                     console.log("Rows: " + rows)
+                    let encrpPassword = crypto.createHash('sha1').update(req.body.password).digest('hex')
+                    console.log("Encrp Password: " + encrpPassword)
                     res.send(rows)
                 }
             }
-            else{
+            else {
                 console.log(err)
                 res.send(err)
             }
@@ -30,12 +32,12 @@ module.exports = {
 
 
 // Table create
-// CREATE TABLE `studentdb`.`auth_users` (
-//     `users_id` INT NOT NULL AUTO_INCREMENT,
-//     `user_name` VARCHAR(50) NULL,
-//     `password` VARCHAR(36) NULL,
-//     PRIMARY KEY (`users_id`),
-//     UNIQUE INDEX `users_id_UNIQUE` (`users_id` ASC) VISIBLE,
-//     UNIQUE INDEX `user_name_UNIQUE` (`user_name` ASC) VISIBLE);
+// CREATE TABLE studentdb.auth_users (
+//     users_id INT NOT NULL AUTO_INCREMENT,
+//     user_name VARCHAR(50) NULL,
+//     password VARCHAR(36) NULL,
+//     PRIMARY KEY (users_id),
+//     UNIQUE INDEX users_id_UNIQUE (users_id ASC) VISIBLE,
+//     UNIQUE INDEX user_name_UNIQUE (user_name ASC) VISIBLE);
 
-// INSERT INTO `studentdb`.`auth_users` (`user_name`, `password`) VALUES ('supriya', '1234');
+// INSERT INTO studentdb.auth_users (user_name, password) VALUES ('supriya', '1234');
