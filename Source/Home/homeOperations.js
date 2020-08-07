694759ca6d07766a8d0bb24b19b07baf0234726b
skvk18 <supriya.kundu.1999@gmail.com>
2020-08-07 10:18:24 +0530

    Page render done

--- Source/Home/homeOperations.js
+++ Source/Home/homeOperations.js
@@ -1,12 +1,14 @@
 const connection = require("../../Database/dbConnection");
 const {authenticatedUser} = require('../Auth/authOperations')
+const path = require('path')
 
 module.exports = {
     "renderHomePage": function (req, res){
         authenticatedUser(req).then((data) => {
             console.log(data)
             if(data.is_authenticated){
-                res.send("Home Page <br>Wellcome "+data.userName)
+                res.sendFile('home.html', {root: path.join(__dirname,'Page')})
+                // res.send("Home Page <br>Wellcome "+data.userName)
             }
             else{
                 res.send("Home Page") 
