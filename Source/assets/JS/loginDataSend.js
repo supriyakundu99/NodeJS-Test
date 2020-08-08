0017e1e83539d7e10253dbd4e2dd38e0d5cc7e32
supriyakundu1999 <supriya.kundu.1999@gmail.com>
2020-08-08 13:12:49 +0530

    Login using ajax

--- Source/assets/JS/loginDataSend.js
+++ Source/assets/JS/loginDataSend.js
@@ -1,3 +1,19 @@
 function doLogin(){
     console.log("DoLogin called....")
+    $.ajax({
+        type: "POST",
+        url: "/account/login",
+        data: {
+            "userName": document.getElementById("userName").value,
+            "password": document.getElementById("password").value
+        },
+        success: (res) => {
+            console.log("success.....");
+            console.log(res)
+        },
+        error: function (res) {
+            console.log("Error...");
+            console.log(res);
+        }
+    });
 }
