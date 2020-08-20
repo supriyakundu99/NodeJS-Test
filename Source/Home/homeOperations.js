fd173664d36da4338bd42be3608abcf3602f146f
Supriya Kundu <supriya.kundu.1999@gmail.com>
2020-08-20 19:12:47 +0530

    Code formatted

--- Source/Home/homeOperations.js
+++ Source/Home/homeOperations.js
@@ -1,20 +1,20 @@
 const connection = require("../../Database/dbConnection");
-const {authenticatedUser} = require('../Auth/authOperations')
+const { authenticatedUser } = require('../Auth/authOperations')
 const path = require('path')
 
 module.exports = {
-    renderHomePage: function (req, res){
-        res.sendFile('home.html', {root: path.join(__dirname,'Page')})
+    renderHomePage: function (req, res) {
+        res.sendFile('home.html', { root: path.join(__dirname, 'Page') })
     },
 
     fetchUserDetails: function (req, res) {
         authenticatedUser(req).then((data) => {
             console.log(data)
-            if(data.is_authenticated){
+            if (data.is_authenticated) {
                 res.json(data)
             }
-            else{
-                res.json(data) 
+            else {
+                res.json(data)
             }
         })
     }
