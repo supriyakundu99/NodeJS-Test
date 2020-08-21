9908d0823d87f475d6bc0c0df45c95bb3be538e6
Supriya Kundu <supriya.kundu.1999@gmail.com>
2020-08-21 17:19:34 +0530

    CSRF token delivery done

--- Router/accounts.js
+++ Router/accounts.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const router = express.Router()
 const { login, register, renderLoginPage, renderRegisterPage } = require('../Source/Auth/authOperations')
-
+const { deliverToken } = require('../Source/Auth/csrfTokenDeliver')
 
 router.get('/login', renderLoginPage)
 router.post('/login', login)
 
 router.get('/register', renderRegisterPage)
 router.post('/register', register)
+
+router.get('/getCSRFToken', deliverToken)
 
 module.exports = router
--- Source/Auth/authOperations.js
+++ Source/Auth/authOperations.js
@@ -155,5 +155,5 @@ module.exports = {
                 resolve(resultObj)
             }
         })
-    }
+    },
 }
