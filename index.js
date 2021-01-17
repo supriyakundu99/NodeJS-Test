603a5dd19342789941950d719f30f3ec20be7a3b
Supriya Kundu <supriya.kundu.1999@gmail.com>
2021-01-17 09:55:07 +0530

    const used for cors

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-var cors = require('cors')
+const cors = require('cors')
 
 const rootRouter = require('./Router/root')
 const cookieParser = require('cookie-parser')
