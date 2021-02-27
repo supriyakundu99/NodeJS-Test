4ed2c6debc073429894501145f668369f7fe9073
Supriya Kundu <supriya.kundu.1999@gmail.com>
2021-02-27 15:56:19 +0530

    Docker db used

--- Database/dbConnection.js
+++ Database/dbConnection.js
@@ -2,10 +2,10 @@ const mysql = require('mysql')
 
 const connection = mysql.createConnection({
     host: '192.168.0.3',
-    port: 3307,
+    port: 3306,
     user: 'Supriya',
     password: 'sk1234',
-    database: 'StudentDB',
+    database: 'studentdb',
 
 })
 
