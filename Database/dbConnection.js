7762d883e6701bba708c36673e039324d95b4552
Supriya Kundu <supriya.kundu.1999@gmail.com>
2021-05-04 21:15:21 +0530

    Dotenv used for DB

--- Database/dbConnection.js
+++ Database/dbConnection.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql')
+require('dotenv').config()
 
 const connection = mysql.createConnection({
-    host: '192.168.0.3',
-    port: 3306,
-    user: 'Supriya',
-    password: 'sk1234',
-    database: 'student_db',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USERMANE,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 
 })
 
